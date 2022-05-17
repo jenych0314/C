8059eb59379c2441f66b64d3b1bacba1851001d4
jenych0314 <jeonych305@gmail.com>
2022-05-17 16:05:00 +0900

    integrated

--- SNAKEGAME/test3/main.cpp
+++ SNAKEGAME/test3/main.cpp
@@ -1,28 +1,14 @@
 #include <iostream>
-<<<<<<< HEAD
-=======
-// #include <ncurses.h>
->>>>>>> b928e3b7006d26b025c4d98065b4c3427d3d4dcd
 #include <curses.h>
 
 using namespace std;
 
-<<<<<<< HEAD
-int main() {
+int main()
+{
     initscr();
     printw("Hello World");
     refresh();
     getch();
     endwin();
     return 0;
-=======
-int main()
-{
-    // cout << "Hello World!";
-    initscr();
-    printw("Hello World!");
-    refresh();
-    getch();
-    endwin();
->>>>>>> b928e3b7006d26b025c4d98065b4c3427d3d4dcd
 }
