64818279cd81b6f0a2e8c76a3bf0348ddbaaede0
jenych0314 <jeonych305@gmail.com>
2022-05-17 11:20:52 +0900

    update

---
+++ SNAKEGAME/test3/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ncurses.h>
+#include <curses.h>
+
+using namespace std;
+
+int main()
+{
+    // cout << "Hello World!";
+    initscr();
+    printw("Hello World!");
+    refresh();
+    getch();
+    endwin();
+}
